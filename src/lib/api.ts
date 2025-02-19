0631f6b3f592a52daa8a9f1fa73aea94c181d535
Leonardo Coelho <leonardo.coelho@experienceone.com>
2025-02-19 09:06:56 +0100

    adding loading

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,19 @@
 import { Post } from "@/types/post";
 
+// Simulate delay in fetching data from API
+const simulateDelay = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 export async function getPosts(): Promise<Post[]> {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  await simulateDelay(2000);
   if (!res.ok) throw new Error("Error when searching for posts");
   return res.json();
 }
 
 export async function getPost(id: string) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  await simulateDelay(2000);
   if (!res.ok) throw new Error("Error when searching for post");
   return res.json();
 }
